bc7ced483a1a8792a0771f6f8cef9eb1010152d8
euvitordev <euvitordev@gmail.com>
2024-06-09 21:09:40 -0300

    feat: Finalize the initial layout of the home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,88 @@
-import Image from "next/image";
+import { DayState } from "@/components/dayState"
+import { Button, Card, CardBody, CardHeader } from "@nextui-org/react"
+import { Plus, Trash } from "lucide-react"
+import Link from "next/link"
 
 export default function Home() {
+  const weekDays = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"]
+  const today = new Date()
+  const todayWeekDay = today.getDay()
+  const sortedweekDays = weekDays
+    .slice(todayWeekDay + 1)
+    .concat(weekDays.slice(0, todayWeekDay + 1))
+
+  const habits = {
+    "Beber água": {
+      "2024-06-09": true,
+      "2024-06-10": false,
+      "2024-06-11": false,
+      "2024-06-12": false,
+      "2024-06-13": false,
+      "2024-06-14": false,
+      "2024-06-15": false,
+    },
+    Correr: {
+      "2024-06-09": true,
+      "2024-06-10": false,
+      "2024-06-11": true,
+      "2024-06-12": false,
+      "2024-06-13": false,
+      "2024-06-14": true,
+      "2024-06-15": false,
+    },
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="flex flex-col items-center justify-between w-screen h-[90vh] py-16 text-center">
+      <div className="flex flex-col items-center gap-8 w-full px-4">
+        {habits === null ||
+          (Object.keys(habits).length === 0 && (
+            <h2>Você não tem hábitos cadastrados</h2>
+          ))}
+
+        {habits !== null &&
+          Object.entries(habits).map(([habit, habitStrik]) => (
+            <Card key={habit} className="w-full">
+              <CardHeader className="border-b border-zinc-200">
+                <div className="flex items-center w-full justify-between">
+                  <h2 className="font-medium">{habit}</h2>
+                  <Button size="sm" isIconOnly variant="flat" color="danger">
+                    <Trash size={20} />
+                  </Button>
+                </div>
+              </CardHeader>
+              <CardBody>
+                <section className="grid grid-cols-7 gap-2">
+                  {sortedweekDays.map((day) => (
+                    <div
+                      key={day}
+                      className="flex flex-col gap-2 items-center justify-center last:font-bold"
+                    >
+                      <span className=" flex items-center justify-center ">
+                        {day}
+                      </span>
+
+                      <DayState day={undefined} />
+                    </div>
+                  ))}
+                </section>
+              </CardBody>
+            </Card>
+          ))}
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="bg-zinc-50 w-full flex items-center justify-center fixed bottom-0 p-2">
+        <Link
+          href={"/novo-habito"}
+          className="flex flex-col items-center justify-center w-44 p-2 gap-1"
+        >
+          <Button color="success" variant="flat" size="md" radius="full">
+            <Plus />
+          </Button>
+          <span className=""> Novo hábito</span>
+        </Link>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* <AddNewHabit /> */}
     </main>
-  );
+  )
 }
